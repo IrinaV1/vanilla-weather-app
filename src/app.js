322f9be015f50d8a4f859f8b56cff595f0076cee
Irina <vyustirina@gmail.com>
2023-09-18 23:56:35 +0300

    Showing the temperature

--- src/app.js
+++ src/app.js
@@ -1,1 +1,21 @@
-alert("hello")
+function displayTemperature(response) {
+    console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    let cityElement = document.querySelector("#city");
+    let descriptionElement = document.querySelector("#description");
+    let humidityElement = document.querySelector("#humidity");
+    let windElement = document.querySelector("#wind");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidityElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+}
+
+
+let apiKey = "f9a03396395e77596cdd41ea58a7f663";
+let city = "New York"
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=${apiKey}&units=metric`;
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
